315ce4157f29b7af8c0ad5cfe8bde97c1fecb39b
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2020-12-22 20:32:19 -0600

    change from absolute to relative path in MainExercise

--- lib/windowsLesson/mainTab/lesson/exercise/MainExercise.dart
+++ lib/windowsLesson/mainTab/lesson/exercise/MainExercise.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn_chatino/windowsLesson/mainTab/lesson/components/ComponentBody.dart';
+import '../components/ComponentBody.dart';
 import '../HeaderWindowLesson.dart';
 
 class MainExercise extends StatefulWidget {
